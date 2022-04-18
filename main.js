69836556de472bde2dd12a28b2df2beba91dab6b
as2295 <102293152+as2295@users.noreply.github.com>
2022-04-18 16:39:06 +0100

    commmm

--- main.js
+++ main.js
@@ -89,19 +89,12 @@ function renderHTML(data){
 
 }
 
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
+function toggleText(){
+  var x = document.getElementById("Myid");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
+    x.style.display = "none";
   }
 }
 
