e8966a7be4bd60e6154149ad672916466e470e3f
as2295 <102293152+as2295@users.noreply.github.com>
2022-04-18 15:48:02 +0100

    randdcomm

--- main.js
+++ main.js
@@ -89,11 +89,20 @@ function renderHTML(data){
 
 }
 
-$(function() {
-  $('#visible').click(function() {
-    $('invisible').toggleClass("show");
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
 
-  });
-});
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
 
 
